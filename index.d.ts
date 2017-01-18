4215b1df4f203a2345b7d49e584658d04114e354
Daniel Pereira <dpereira@lifesize.com>
2017-01-18 15:25:52 -0600

    Fix type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,30 @@
-declare module 'mock-raf' {
+interface MockRafOptions {
 
-  interface MockRafOptions {
+  /** The time that should pass during each requestAnimationFrame step in milliseconds. Default is roughly equivalent to default browser behavior. */
+  time?: number;
 
-    /** The time that should pass during each requestAnimationFrame step in milliseconds. Default is roughly equivalent to default browser behavior. */
-    time?: number;
+  /** The number of steps to take. */
+  count?: number;
+}
 
-    /** The number of steps to take. */
-    count?: number;
-  }
+/** Creates a mockRaf instance, exposing the functions you'll use to interact with the mock. */
+interface MockRaf {
 
-  interface MockRaf {
+  /** Returns the current now value of the mock. Starts at 0 and increases with each step() taken. Useful for stubbing out performance.now() or a polyfill when using requestAnimationFrame with timers. */
+  now(): number;
 
-    /** Returns the current now value of the mock. Starts at 0 and increases with each step() taken. Useful for stubbing out performance.now() or a polyfill when using requestAnimationFrame with timers. */
-    now(): number;
+  /** Replacement for requestAnimationFrame or a polyfill.Adds a callback to be fired on the next step. */
+  raf(callback: FrameRequestCallback): number;
 
-    /** Replacement for requestAnimationFrame or a polyfill.Adds a callback to be fired on the next step. */
-    raf(callback: FrameRequestCallback): number;
+  /** Replacement for cancelAnimationFrame or a polyfill.Removes all currently scheduled requestAnimationFrame callbacks from the queue. */
+  cancel(handle: number): void;
 
-    /** Replacement for cancelAnimationFrame or a polyfill.Removes all currently scheduled requestAnimationFrame callbacks from the queue. */
-    cancel(handle: number): void;
+  /** Takes requestAnimationFrame steps. Fires currently queued callbacks for each step and increments now time for each step. The primary way to interact with a mockRaf instance for testing. */
+  step(options?: MockRafOptions): void;
+}
 
-    /** Takes requestAnimationFrame steps. Fires currently queued callbacks for each step and increments now time for each step. The primary way to interact with a mockRaf instance for testing. */
-    step(options?: MockRafOptions): void;
-  }
+declare const mockRaf: () => MockRaf;
 
-  /** Creates a mockRaf instance, exposing the functions you'll use to interact with the mock. */
-  export default function createMockRaf(): MockRaf;
+declare module "mock-raf" {
+  export = mockRaf;
 }
